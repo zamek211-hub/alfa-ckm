941e70ee38d0fa4dcf6ad513b69a622b8349601c
Jan Lap <zamek211@gmail.com>
2026-01-05 20:42:08 +0100

    fix: move contact API to app/api/contact

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,5 +1,7 @@
 import nodemailer from "nodemailer";
 
+export const runtime = "nodejs";
+
 export async function POST(req: Request) {
   try {
     const { name, email, message } = await req.json();

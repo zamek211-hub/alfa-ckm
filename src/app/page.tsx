3d2cd91247bf87b905f103cba674b9fc0ef507e3
Jan Lap <zamek211@gmail.com>
2026-01-12 16:01:09 +0100

    DEBUG: force hero logo from /assets/hero with red border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,13 +23,10 @@ export default function HomePage() {
       >
         {/* 🔰 LOGO KLUBU – LEWY GÓRNY RÓG */}
         <div className="absolute top-6 left-6 z-20">
-        <Image
+       <img
   src="/assets/hero/alfa-ckm-logo.png"
-  alt="ALFA-CKM Logo"
-  width={600}
-  height={600}
-  priority
-  className="w-[120px] md:w-[160px] lg:w-[200px] opacity-90"
+  alt="ALFA-CKM HERO LOGO"
+  style={{ border: "5px solid red" }}
 />
         </div>
 

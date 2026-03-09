9e64926887b4cd9f19866f35bc51bac11acf0ed1
zamek211-hub <zamek211@gmail.com>
2026-03-09 23:49:05 +0100

    add redirect members -> czlonkowie

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/members",
+        destination: "/czlonkowie",
+        permanent: true,
+      },
+    ];
+  },
+
   //reactCompiler: true,
 };
 

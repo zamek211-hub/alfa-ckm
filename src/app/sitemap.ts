cd1c4a88d55e7a28e296b8465cf467099c4282a9
zamek211-hub <zamek211@gmail.com>
2026-03-13 00:42:49 +0100

    fix sitemap remove media scan

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+export default function sitemap() {
+  const baseUrl = "https://www.alfackm.pl";
+
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/o-nas`, lastModified: new Date() },
+    { url: `${baseUrl}/czlonkowie`, lastModified: new Date() },
+    { url: `${baseUrl}/partnerzy`, lastModified: new Date() },
+    { url: `${baseUrl}/partnerzy/zostan-partnerem`, lastModified: new Date() },
+    { url: `${baseUrl}/kontakt`, lastModified: new Date() },
+    { url: `${baseUrl}/statut`, lastModified: new Date() }
+  ];
+}

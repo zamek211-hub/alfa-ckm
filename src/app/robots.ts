6faf36debe58ccc0b58f08b85cf7b33299838e70
zamek211-hub <zamek211@gmail.com>
2026-03-13 01:03:49 +0100

    fix sitemap build

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,17 +1,17 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = "https://www.alfackm.pl";
+
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/_next/",
-        ],
-      },
-    ],
-    sitemap: "https://www.alfackm.pl/sitemap.xml",
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/api/",
+        "/_next/",
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
